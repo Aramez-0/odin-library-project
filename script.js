84b6503b780db8a1a0c90f90759d57006e7e528f
Aramez <191219550+Aramez-0@users.noreply.github.com>
2025-02-06 05:02:50 +1000

    Added remove and edit book buttons

--- script.js
+++ script.js
@@ -19,9 +19,10 @@ function addBookToLibrary(title, author, pages, read) {
 };
 
 
-function displayLibrary(item) {
+function displayLibrary(item, id) {
     let div = document.createElement("div");
     div.setAttribute("class", "book-item");
+    div.setAttribute("id", `${id}`)
     container.appendChild(div);
 
     const fields = [
@@ -35,6 +36,54 @@ function displayLibrary(item) {
         let pElement = document.createElement("p");
         pElement.textContent = `${field.label}: ${field.value}`;
         div.appendChild(pElement);
+    });
+
+    let btn = document.createElement("button");
+    btn.textContent = 'Delete Book';
+    div.appendChild(btn);
+    btn.setAttribute("class", "delete-book");
+    btn.setAttribute("type", "button");
+
+    btn.addEventListener("click", () => {
+        div.remove();
+    })
+
+    let btn2 = document.createElement("button");
+    btn2.textContent = 'Edit Book';
+    div.appendChild(btn2);
+    btn2.setAttribute("class", "edit-book");
+    btn2.setAttribute("type", "button");
+
+    btn2.addEventListener("click", () => {
+        newBookContainer.style = "display: block;";
+        bookTitle.value = item.title;
+        bookAuthor.value = item.author;
+        bookPages.value = item.pages;
+        bookStatus.value = item.read;
+
+        submitBook.removeEventListener("click", submitEditBook);
+
+        submitBook.addEventListener("click", submitEditBook);
+
+        function submitEditBook() {
+            div.remove();
+            
+            item.title = bookTitle.value;
+            item.author = bookAuthor.value;
+            item.pages = bookPages.value;
+            item.read = bookStatus.value;
+
+            div.querySelector("p:nth-child(1)").textContent = `Title: ${item.title}`;
+            div.querySelector("p:nth-child(2)").textContent = `Author: ${item.author}`;
+            div.querySelector("p:nth-child(3)").textContent = `Pages: ${item.pages}`;
+            div.querySelector("p:nth-child(4)").textContent = `Read: ${item.read}`;
+
+            bookTitle.value = '';
+            bookAuthor.value = '';
+            bookPages.value = '';
+            bookStatus.value = 'Reading';
+            newBookContainer.style = "display: none;";
+        }
     });
 };
 
@@ -57,10 +106,9 @@ let submitBook = document.querySelector("#submit-book");
 
 
 submitBook.addEventListener("click", () => {
-    console.log(bookTitle.value, bookAuthor.value, bookPages.value, bookStatus.value);
     addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value, bookStatus.value);
     let newBook = myLibrary[myLibrary.length - 1];
-    displayLibrary(newBook);
+    displayLibrary(newBook, myLibrary.length - 1);
     bookAuthor.value = '';
     bookPages.value = '';
     bookTitle.value = '';
